5adc369061ddada8471adeb31cb4869e39474aee
assanediallo <76051408+assanediallo@users.noreply.github.com>
2022-03-31 14:18:14 +0200

    Create Connecte.java

---
+++ Environnement/Connecte.java
@@ -0,0 +1,35 @@
+package Environnement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Connecte {
+	
+	//Connect("");
+	public static StringBuffer envoi(String urlend) throws IOException {
+		String str ="http://10.3.2.121:8000/"+ urlend	;
+		URL obj = new URL (str);
+				
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+		if (responseCode == HttpURLConnection.HTTP_OK) { // success
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+	
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			return response;
+		} else {
+			return null;
+		}	
+	}
+}
