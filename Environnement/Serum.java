82f8dcec8fc40bdcb5d2b9b4b5b44af0704d9a26
assanediallo <76051408+assanediallo@users.noreply.github.com>
2022-03-31 14:21:09 +0200

    Create Serum.java

---
+++ Environnement/Serum.java
@@ -0,0 +1,11 @@
+package Environnement;
+
+/**
+ * soin est le type de soin testé
+ * @author assane
+ * serumPhy	est un booléen qui, s'il est à vrai, indique que le patient est soigné avec un traitement tel que le sérum physiologique : un soin témoin pour chiffrer l'effet placebo.
+ */
+public class Serum {
+	Medicament soin;
+	boolean serumPhy;
+}
